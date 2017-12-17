9f5463bc0e53e0455df2ad69624a8cacae3c92ac
Gabriel West <gabe_west1@yahoo.com>
2017-12-17 00:49:20 -0600

    Updated <App/> to include a navbar.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from "styled-components"
+import './App.css'
 
 import ParticlesBackground from "./components/ParticlesBackground"
 
+const Header = styled.header`
+	align-items: center;
+	display: flex;
+	height: 350px;
+	justify-content: center;
+	position: relative;
+`
+const Quote = styled.p`
+	text-align: center;
+`
+const _ParticlesBackground = styled(ParticlesBackground)`
+	background-color: #112128;
+	position: absolute;
+	z-index: -1;	//Lets the quote hover above
+`
+const Underline = styled.span`
+	text-decoration: underline;
+`
+const Navbar = styled.nav`
+	display: flex;
+	font-size: .75em;
+	justify-content: space-between;
+	padding: 0 1em;
+	position: absolute;
+	top: 0;
+	width: 100%;
+	box-sizing: border-box;
+`
+const List = styled.div`
+	display: flex;
+	
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	* {
+		align-items: center;
+		display: flex;
+		list-style: none;
+		margin-left: 1em;
+		margin-right: 1em;
+	}
+
+	*:last-child {
+		margin-right: 0;
+	}
+`
 class App extends Component {
   render() {
     return (
-      <ParticlesBackground />
-    );
+		<Header>
+			<_ParticlesBackground />
+			<Navbar>
+				<p>Gabe West</p>
+				<List>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Contact</a></li>
+				</List>
+			</Navbar>
+			<Quote>
+				Imagination is more important the knowledge
+				<br/> For knowledge is <Underline>limited,</Underline>
+				<br/> whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.
+			</Quote>
+		</Header>
+    )
   }
 }
 
-export default App;
+export default App
