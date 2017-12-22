79006fa3839f9c69782128fadbeb9eca8e6f50a4
Gabriel West <gabe_west1@yahoo.com>
2017-12-21 20:58:06 -0600

    Added periods to the end of the adjectives.

--- src/components/shared/SelfWritingText/adjectives.js
+++ src/components/shared/SelfWritingText/adjectives.js
@@ -1,8 +1,8 @@
 const adjectives = [
-    "Software Engineer",
-    "Mobile Developer",
-    "Fullstack Developer",
-    "Thinker"
+    "Software Engineer.",
+    "Mobile Developer.",
+    "Fullstack Developer.",
+    "Thinker."
 ]
 
 export default adjectives
