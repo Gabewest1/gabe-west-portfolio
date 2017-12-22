3a91f782c9c6150df5e33ddd0e4160ba65b5670a
Gabriel West <gabe_west1@yahoo.com>
2017-12-21 20:17:08 -0600

    Updated the projectData to include a sub title.

---
+++ src/components/Projects/projectData.js
@@ -0,0 +1,58 @@
+const projectData = [
+    { 
+        title: "Simon Says",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Calculator",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Weather Forecast",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Wikipedia Viewer",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Tic-Tac-Toe",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Batman Tribute Page",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Twitch TV",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Youtube",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+    { 
+        title: "Dell",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod,placeat incidunt architecto quae quasi rem, magnam quidem sapiente autem suscipit. Harum eveniet quibusdam aut ex!",
+        src: "/assets/images/calculator.jpg",
+        subTitle: "This is the subtitle"
+    },
+]
+
+export default projectData
