f31bf96a16c6c160963a6c01aa85e12c030947ee
Gabriel West <gabe_west1@yahoo.com>
2017-12-25 02:38:33 -0600

    Removed <ParticlesBackground/>

--- src/App.js
+++ src/App.js
@@ -5,22 +5,15 @@ import './App.css'
 import Header from "./components/Header"
 import Projects from "./components/Projects"
 import About from "./components/About"
-import ParticlesBackground from "./components/shared/ParticlesBackground"
 
 const Container = styled.div`
 
-`
-const _ParticlesBackground = styled(ParticlesBackground)`
-	background-color: #112128;
-	position: fixed;
-	z-index: -1;	//Lets the quote hover above
 `
 
 class App extends Component {
   render() {
     return (
 		<Container>
-			<_ParticlesBackground />
 			<Header />
 			<About />
 			<Projects />
