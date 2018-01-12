2a4517eb326a1c921f584da1e888ccbccc53e00e
Gabriel West <gabe_west1@yahoo.com>
2018-01-12 16:37:21 -0600

    Updated <SectionTitle/> to display both left and right stripes.

--- src/components/shared/SectionTitle/index.js
+++ src/components/shared/SectionTitle/index.js
@@ -5,26 +5,37 @@ import { PRIMARY_COLOR, SECONDARY_COLOR } from "../../../constants"
 const Stripe = styled.div`
     position: absolute;
     top: 0;
-    border-top: solid 150px transparent;
+    border-top: solid 12vmax transparent;
     
     /* GENERATE LEFT/RIGHT POSITION AND STRIPE DIRECTION */
     ${({ right, secondary }) => {
         const position = right ? "right: 0" : "left: 0"
-        const borderColor = secondary ? SECONDARY_COLOR : PRIMARY_COLOR
+        // const borderColor = secondary ? SECONDARY_COLOR : PRIMARY_COLOR
+        const borderColor = "#718199"
         const borderDirection = right ? "border-right" : "border-left"
 
         return `
             ${position};
-            ${borderDirection}: solid 285px ${ borderColor };
+            ${borderDirection}: solid 12vmax ${ borderColor };
         `
     }}
 `
 const Title = styled.h3`
     line-height: 12vmax;
-    font-size: 28px;
+    font-size: 20px;
     text-align: center;
     letter-spacing: 4px;
     margin: 0;
+    position: relative;
+    z-index: 1;
+
+    @media (min-width: 768px) {
+        font-size: 28px;
+    }
+`
+const SectionTitle = styled.div`
+    position: relative;
+    overflow: hidden;
 
     ${({ right, secondary }) => {
         const backgroundColor = secondary ? PRIMARY_COLOR : SECONDARY_COLOR
@@ -33,14 +44,11 @@ const Title = styled.h3`
         return `background-color: ${ backgroundColor }; color: ${ color };`
     }}
 `
-const SectionTitle = styled.div`
-    position: relative;
-    overflow: hidden;
-`
 
 export default (props) => (
     <SectionTitle { ...props }>
-        <Stripe right={ props.right } secondary={ props.secondary } />
+        { props.right && <Stripe right={ true } secondary={ props.secondary } /> }
+        { props.left && <Stripe right={ false } secondary={ props.secondary } /> }
         <Title { ...props }>{ props.children }</Title>
     </SectionTitle>
 )
