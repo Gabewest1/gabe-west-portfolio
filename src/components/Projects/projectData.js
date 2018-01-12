3942eafabf426a239dc876556b4357737e6439f8
Gabriel West <gabe_west1@yahoo.com>
2018-01-11 22:11:39 -0600

    Updated project data by adding subTitles.

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -3,55 +3,55 @@ const projectData = [
         title: "Simon Says",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/simon_says.png",
-        subTitle: "This is the subtitle"
+        subTitle: "React Native & Socket.io"
     },
     { 
         title: "Tic-Tac-Toe",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/tic_tac_toe.png",
-        subTitle: "This is the subtitle"
+        subTitle: "React, Redux & Socket.io"
     },
     { 
         title: "Weather Forecast",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/weather_app.png",
-        subTitle: "This is the subtitle"
+        subTitle: "React & Redux"
     },
     { 
         title: "Quote Generator",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/quote_generator.png",
-        subTitle: "This is the subtitle"
+        subTitle: "React & Redux"
     },
     { 
         title: "Wikipedia Viewer",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/wiki_viewer.png",
-        subTitle: "This is the subtitle"
+        subTitle: "React & Redux"
     },
     { 
         title: "Calculator",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/calculator.jpg",
-        subTitle: "This is the subtitle"
+        subTitle: "React & Redux"
     },
     { 
         title: "Twitch TV",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/twitch_tv_app.png",
-        subTitle: "This is the subtitle"
+        subTitle: "React & Redux"
     },
     { 
         title: "Youtube Homepage",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/youtube.png",
-        subTitle: "This is the subtitle"
+        subTitle: "Vanilla JS"
     },
     { 
         title: "Dell Homepage",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/dell.png",
-        subTitle: "This is the subtitle"
+        subTitle: "Angular"
     },
 ]
 
