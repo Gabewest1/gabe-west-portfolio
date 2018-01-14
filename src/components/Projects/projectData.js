1289a89b5d44fd054eeca507460eb38277d2e0d2
Gabriel West <gabe_west1@yahoo.com>
2018-01-13 20:10:49 -0600

    Updated project data to include urls to the projects.

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -3,54 +3,67 @@ const projectData = [
         title: "Simon Says",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/simon_says.png",
-        subTitle: "React Native & Socket.io"
+        href: {
+            android: "https://play.google.com/store/apps/details?id=com.cimon_says_online",
+            ios: "https://itunes.apple.com/us/app/App-Name/id1323885065"
+        },
+        subTitle: "React Native & Socket.io",
+        reactNative: true
     },
     { 
         title: "Tic-Tac-Toe",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/tic_tac_toe.png",
+        href: "https://ultimate-online-tic-tac-toe.herokuapp.com/",
         subTitle: "React, Redux & Socket.io"
     },
     { 
         title: "Weather Forecast",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/weather_app.png",
+        href: "https://gabes-weather-app.herokuapp.com/",
         subTitle: "React & Redux"
     },
     { 
         title: "Quote Generator",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/quote_generator.png",
+        href: "https://trump-admin-quote-generator.herokuapp.com/",
         subTitle: "React & Redux"
     },
     { 
         title: "Wikipedia Viewer",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/wiki_viewer.png",
+        href: "https://react-wiki-viewer.herokuapp.com/",
         subTitle: "React & Redux"
     },
     { 
         title: "Calculator",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/calculator.jpg",
+        href: "https://react-redux-calculator.herokuapp.com/",
         subTitle: "React & Redux"
     },
     { 
         title: "Twitch TV",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/twitch_tv_app.png",
+        href: "https://twitch-tv-app-react.herokuapp.com/",
         subTitle: "React & Redux"
     },
     { 
         title: "Youtube Homepage",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/youtube.png",
+        href: "https://youtube-homepage.herokuapp.com/",
         subTitle: "Vanilla JS"
     },
     { 
         title: "Dell Homepage",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis illo veritatis consectetur alias quod!",
         src: "/assets/images/dell.png",
+        href: "https://dell-homepage.herokuapp.com/",
         subTitle: "Angular"
     },
 ]
