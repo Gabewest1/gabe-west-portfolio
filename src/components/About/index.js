bc5b3611721a61c92270f2efc4fef35739411e3e
Gabriel West <gabe_west1@yahoo.com>
2018-01-18 20:56:55 -0600

    Updated styles of the <Skill/> and rearranged the order.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -26,19 +26,20 @@ export default class About extends React.PureComponent {
                 </AboutMeHeader>
 
                 <Main>
-                    <JavascriptSkill>
-                        <Skill>
-                            <Icon>
-                                <img src="/assets/images/javascript.png" alt=""/>
-                            </Icon>
-                            <h3>Creating Amazing experiences</h3>
-                            <p>Illo velit doloremque laudantium dolorum aliquid blanditiis vero quas facilis.</p>
-                        </Skill>
-                    </JavascriptSkill>
-
-                    <BarConnectingSkills style={ bar } />
 
                     <Skills>
+                        <JavascriptSkill>
+                            <Skill>
+                                <Icon>
+                                    <img src="/assets/images/skills.png" alt=""/>
+                                </Icon>
+                                <h3>Creating Amazing Experiences</h3>
+                                <p>With the power of JavaScript, I can do ANYTHING!</p>
+                            </Skill>
+                        </JavascriptSkill>
+
+                        <BarConnectingSkills style={ bar } />
+
                         <OtherSkills>
                             <Skill innerRef={ leftSkill => this.leftSkill = leftSkill}>
                                 <Icon>
@@ -58,25 +59,24 @@ export default class About extends React.PureComponent {
 
                             <Skill>
                                 <Icon>
-                                    <img src="/assets/images/database_icon.png" alt=""/>
-                                </Icon>
-                                <h3>Handling Big Data</h3>
-                                <p>Manage vasts amounts of user-critical information</p>
-                            </Skill>
-
-                            <Skill innerRef={ rightSkill => this.rightSkill = rightSkill }>
-                                <Icon>
                                     <img src="/assets/images/node.png" alt=""/>
                                 </Icon>
                                 <h3>Communicating Across the Web</h3>
                                 <p>Orchastrate communications between client and server</p>
                             </Skill>
+
+                            <Skill innerRef={ rightSkill => this.rightSkill = rightSkill }>
+                                <Icon>
+                                    <img src="/assets/images/database_icon.png" alt=""/>
+                                </Icon>
+                                <h3>Handling Big Data</h3>
+                                <p>Manage vasts amounts of user-critical information</p>
+                            </Skill>
                         </OtherSkills>
                     </Skills>
                 </Main>
 
-                <div style={{ padding: "0 20px 40px 20px", width: "100%", boxSizing: "border-box" }}>
-                    {/* <p><em>Simply put:</em> To be the best developer I can possible be.</p> */}
+                <div>
                     <MissionStatement>
                         I want to be able to construct any idea in my head through code. I don't want to constrict my ability to create
                         awesome applications because of a limited technical know-how.
@@ -91,13 +91,13 @@ export default class About extends React.PureComponent {
         )
     }
     componentWillMount = () => {
-        window.addEventListener("scroll", this._positionBarComponent)
+        window.addEventListener("resize", this._positionBarComponent)
     }
     componentDidMount = () => {
         this._positionBarComponent()
     }
     componentWillUnmount = () => {
-        window.removeEventListener("scroll", this._positionBarComponent)        
+        window.removeEventListener("resize", this._positionBarComponent)        
     }
     _positionBarComponent = () => {
         const leftSkill = this._getElementsPosition(this.leftSkill)
@@ -105,7 +105,7 @@ export default class About extends React.PureComponent {
         
         const top = leftSkill.top - BAR_OFFSET
         const left = leftSkill.left - BAR_WIDTH
-        const right = rightSkill.left + BAR_WIDTH
+        const right = rightSkill.left - BAR_WIDTH
         const width = (right - left) + "px"
 
         this.setState({ bar: { left, top, width }})
@@ -116,12 +116,12 @@ export default class About extends React.PureComponent {
         const left = element.offsetLeft
         const top = element.offsetTop
 
-        return { left: (width / 2) + left, top }
+        return { left: (width / 2) + left, top, width, height }
     }
 }
 
 const BAR_WIDTH = 2
-const BAR_HEIGHT = 30
+const BAR_HEIGHT = 40
 const BAR_OFFSET = 40
 
 const BarConnectingSkills = styled.div`
@@ -129,9 +129,18 @@ const BarConnectingSkills = styled.div`
     height: 5px;
     position: absolute;
 `
+const Icon = styled.div`
+    width: 80px;
+    height: 80px;
+
+    img {
+        max-width: 100%;
+    }
+`
 const OtherSkills = styled.div`
     display: flex;
     flex-wrap: wrap;
+    width: 100%;
 `
 const JavascriptSkill = styled.div`
     display: flex;
@@ -146,6 +155,8 @@ const Skill = styled.li`
     margin-bottom: 44px;
     margin: 0;
     position: relative;
+    padding: 0 5px;
+    box-sizing: border-box;
 
     h3 {
         margin-bottom: 5px;
@@ -154,12 +165,9 @@ const Skill = styled.li`
     p {
         color: ${ colorer(PRIMARY_COLOR).light(-10)};
         margin: 0;
-        // max-width: 90%;
     }
 `
 const Skills = styled.ul`
-    display: flex;
-    flex-wrap: wrap;
     list-style: none;
     font-size: 13px;
     padding: 0;
@@ -172,6 +180,12 @@ const Skills = styled.ul`
         margin-top: 90px;
 
         ${Skill} {
+            margin-bottom: ${ BAR_HEIGHT + (BAR_OFFSET / 2) }px;
+
+            ${Icon} {
+                margin-top: 10px;
+            }
+
             &:after {
                 content: "";
                 position: absolute;
@@ -190,10 +204,14 @@ const Skills = styled.ul`
                 position: absolute;
                 height: ${ BAR_HEIGHT }px;
                 border: solid ${ BAR_WIDTH }px ${ PRIMARY_COLOR };
-                bottom: -${ BAR_OFFSET }px;
+                bottom: -${ BAR_OFFSET + 10 }px;
                 left: 50%;
                 transform: translateX(-50%);
             }
+        }
+
+        ${Icon} {
+            width: 120px;
         }
     }
     @media (min-width: 768px) {
@@ -201,14 +219,6 @@ const Skills = styled.ul`
         ${Skill} {
             flex: 0 1 25%;
         }
-    }
-`
-const Icon = styled.div`
-    width: 80px;
-    height: 80px;
-
-    img {
-        max-width: 100%;
     }
 `
 const Underline = styled.span`
@@ -230,10 +240,6 @@ const Main = styled.div`
 
     @media (min-width: 920px) {
         flex-direction: row;
-
-        div:first-child {
-            margin-right: 14px;
-        }
     }
 `
 const Header = styled.div`
@@ -268,7 +274,7 @@ const AboutMeImg = styled.img`
     width: 100%;
 `
 const AboutMeHeader = styled.div`
-    margin-bottom: 60px;
+    margin-bottom: 30px;
 
     h1 {
         margin-bottom: 7px;
